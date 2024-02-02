e6817105d83e3d6cfd65cbc819210e019189e17b
manasa <mmanasa722@gmail.com>
2024-02-02 19:54:26 +0530

    library added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.doc_file_support"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -29,6 +29,18 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/LICENSE")
+        exclude("META-INF/LICENSE.txt")
+        exclude("META-INF/license.txt")
+        exclude("META-INF/NOTICE")
+        exclude("META-INF/NOTICE.txt")
+        exclude("META-INF/notice.txt")
+        exclude("META-INF/ASL2.0")
+        exclude("META-INF/*.kotlin_module")
+    }
 }
 
 dependencies {
@@ -39,6 +51,21 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(files("libs/poi-5.2.5.jar"))
+    implementation(files("libs/poi-ooxml-5.2.5.jar"))
+    implementation(files("libs/poi-scratchpad-5.2.5.jar"))
+    implementation(files("libs/log4j-api-2.21.1.jar"))
+    implementation(files("libs/commons-io-2.15.0.jar"))
+    implementation (files("libs/pdfbox-3.0.1.jar"))
+
+    /*implementation (files("libs/pdfbox-3.0.1.jar"))
+
+    implementation ("org.apache.tika:tika-core:2.3.0")
+    implementation ("org.jsoup:jsoup:1.14.3")*/
+
+
+
 
 
 
--- app/src/main/java/com/example/doc_file_support/MainActivity.java
+++ app/src/main/java/com/example/doc_file_support/MainActivity.java
@@ -1,25 +1,54 @@
 package com.example.doc_file_support;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.TextView;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.hwpf.usermodel.Range;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xslf.util.PDFFontMapper;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,15 +92,22 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_FILE_MANAGER && resultCode == RESULT_OK) {
 
-            webView.setVisibility(View.VISIBLE);
+            /*webView.setVisibility(View.VISIBLE);
             textView.setVisibility(View.GONE);
-            button.setVisibility(View.GONE);
+            button.setVisibility(View.GONE);*/
+
+            Uri uri = data.getData();
+
+
+
+            displayDocFile(String.valueOf(uri));
+           /* displayExcelFile(String.valueOf(uri));*/
 
             /*Uri uri = data.getData();
 
             String result = uri.toString();
             webView.loadUrl("http://docs.google.com/gview?embedded=true&url="+result);*/
-            Uri uri = data.getData();
+            /*Uri uri = data.getData();
 
 
             //.doc file opening in browser where file uploaded in drive the my url link is drive link
@@ -80,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
             String embedUrl = "https://drive.google.com/file/d/" + fileId + "/preview";
             webView.getSettings().setJavaScriptEnabled(true); // Enable JavaScript
             webView.loadUrl(embedUrl);
-
+*/
             /*String myUrl = "https://drive.google.com/drive/folders/1Fx6pU39UhqrznH4DuygqRLaiZxBdq3Rk?usp=drive_link";
             String folderId = myUrl.substring(myUrl.indexOf("/folders/") + 9, myUrl.indexOf("?"));
             String folderUrl = "https://drive.google.com/drive/folders/" + folderId;
@@ -104,4 +140,74 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-}
+
+   /* private void convertDocToDocx(String inputPath) throws Exception {
+        WebView webView1 = findViewById(R.id.webview);
+        webView1.setVisibility(View.VISIBLE);
+
+        if (inputPath.endsWith(".doc")) {
+            // For .doc files
+            HWPFDocument doc = new HWPFDocument(Files.newInputStream(Paths.get(inputPath)));
+            Range range = doc.getRange();
+
+            // Create a new .docx document
+            XWPFDocument docx = new XWPFDocument();
+
+            // Copy content from .doc to .docx
+            for (int i = 0; i < range.numParagraphs(); i++) {
+                String text = range.getParagraph(i).text();
+                docx.createParagraph().createRun().setText(text);
+            }
+
+            // Convert .docx to HTML
+            String htmlContent = convertDocxToHtml(docx);
+
+            // Load HTML content into WebView
+            webView1.loadDataWithBaseURL(null, htmlContent, "text/html", "UTF-8", null);
+            webView1.setWebViewClient(new WebViewClient());
+        } else {
+            System.out.println("Input file is not a .doc file");
+        }
+    }*/
+
+    /*private String convertDocxToHtml(XWPFDocument docx) throws IOException {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+            XHTMLConverter.getInstance().convert(docx, byteArrayOutputStream, null);
+
+            return byteArrayOutputStream.toString();
+        }
+    }*/
+
+
+
+    private void displayDocFile(String fileUriString) {
+        try {
+            WebView webView = findViewById(R.id.webview);
+            webView.setVisibility(View.VISIBLE);
+
+            Uri fileUri = Uri.parse(fileUriString);
+            InputStream inputStream = getContentResolver().openInputStream(fileUri);
+
+            HWPFDocument doc = new HWPFDocument(inputStream);
+            WordExtractor extractor = new WordExtractor(doc);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("<html><body>");
+            for (String paragraph : extractor.getParagraphText()) {
+                sb.append("<p>").append(paragraph).append("</p>");
+            }
+            sb.append("</body></html>");
+
+            inputStream.close();
+
+            webView.loadDataWithBaseURL(null, sb.toString(), "text/html", "UTF-8", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error loading document", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    }
+
+
+
